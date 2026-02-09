0356c9b4b41012d3f54624731a3a7bcbc49a67a2
Stephanie Benita Hannemann-Pena <shannema@student.42heilbronn.de>
2026-02-09 21:40:20 +0100

    ft_printf.c format specifier defined, to be tested

--- ft_printf.c
+++ ft_printf.c
@@ -6,9 +6,51 @@
 /*   By: shannema <shannema@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 15:06:56 by shannema          #+#    #+#             */
-/*   Updated: 2026/02/09 21:18:39 by shannema         ###   ########.fr       */
+/*   Updated: 2026/02/09 21:38:34 by shannema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	handle_format(va_list args, char specifier)
+{
+	if (specifier == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (specifier == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (specifier == 'p')
+		return (ft_putptr(va_arg(args, void *)));
+	else if (specifier == 'd' || specifier == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	else if (specifier == 'u')
+		return (ft_putnbr_unsigned(va_arg(args, unsigned int)));
+	else if (specifier == 'x' || specifier == 'X')
+		return (ft_puthex(va_arg(args, unsigned int), specifier));
+	else if (specifier == '%')
+		return (ft_putchar('%'));
+	return (0);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			count += handle_format(args, format[i + 1]);
+			i++;
+		}
+		else
+			count += ft_putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
